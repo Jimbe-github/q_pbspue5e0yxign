586ea07cdd992641112b9b67be706ae322597798
Jimbe <62501697+Jimbe-github@users.noreply.github.com>
2024-07-02 02:30:16 +0900

    キングがチェックされている時にはそのチェックを外すような移動しか出来ない風にした
    キングがチェック時されていたら各駒のプランから「チェックしている駒とその行動範囲」を抽出する

--- test.js
+++ test.js
@@ -53,7 +53,7 @@ class Piece {
 		this.y = y;
 	}
 	//各方向への直線移動(獲得)プラン
-	getStraightPlans(deltas, gameBoard) {
+	getStraightPlans(deltas, gameBoard, opponentPlanBoard) {
 		const result = [];
 		deltas.forEach(({dx, dy}) => {
 			const route = [];
@@ -64,18 +64,28 @@ class Piece {
 				const plan = new ActionPlan(xx, yy, this, value, route); //移動・獲得・ガード
 				result.push(plan);
 				if(value) {
-					// King をチェックした場合に同じ直線上に移動出来ないようにするため、1つ先の情報も付け足しておく
+					// (相手の)チェックされているキングが逃げ先として同じ直線上に移動出来ないようにするため、
+					// 1つ先の情報も付け足しておく
 					const extraStep = this.player.coordinateCorrection(xx, yy, dx, dy);
 					this.#appendExtraStepTo(plan, extraStep, gameBoard);
 					break;
 				}
 			}
 		});
-		return result;
+		return this.correctionDefense(result, gameBoard, opponentPlanBoard);
 	}
 	#appendExtraStepTo(plan, extraStep, gameBoard) {
 		const {inside, value} = gameBoard.get(extraStep.xx, extraStep.yy);
 		if(inside && !value) plan.extraStep = {x:extraStep.xx, y:extraStep.yy};
+	}
+	//もしキングがチェックされているのならチェックしている駒をどうにか出来るモノだけにする
+	correctionDefense(result, gameBoard, opponentPlanBoard) {
+		let checkedPlan = gameBoard.getCHECK(this.player, opponentPlanBoard); //チェックしている相手の駒のプラン
+		if(!checkedPlan) return result;
+		// checkedPlan のピース自体への攻撃もしくは行動範囲(route)への移動のみ
+		return result.filter(plan =>
+			plan.target === checkedPlan.piece ||
+			checkedPlan.route?.find(({x,y}) => x === plan.x && y === plan.y));
 	}
 }
 
@@ -117,7 +127,12 @@ class GameBoard extends Board {
 		return this._board.filter(v => v.player === player);
 	}
 	getKing(player) {
-		return this._board.find(v => v instanceof King);
+		return this._board.find(v => v instanceof King && v.player === player);
+	}
+	getCHECK(player, opponentPlanBoard) {
+		const king = this.getKing(player);
+		const planArray = opponentPlanBoard?.get(king.x, king.y).value?.arr;
+		return planArray ? planArray[0] : undefined; //チェックしている相手の駒は1つだけのはずなので [0] 決め打ち
 	}
 }
 
@@ -128,12 +143,11 @@ class ActionPlan {
 		this.y = y;
 		this.piece = piece; //アクションするピース
 		this.target = target; //対象ピース(自分の駒だったらガード対象, 相手の駒だったら取られる駒)
-		this.route = route && route.lenth > 0 ? structuredClone(route) : undefined; //ここに来るまでに piece が通る位置{x,y}
+		this.route = route && route.length > 0 ? structuredClone(route) : []; //ここに来るまでに piece が通る位置{x,y}
 		this.captureOnly = false; //true だったら target が空でも(単なる)移動先には出来ない (Pawn の斜め前用)
 	}
 	setCaptureOnly(captureOnly) {
-		this.captureOnly = !!captureOnly;
-		return this;
+		this.captureOnly = !!captureOnly; return this;
 	}
 	isCapture() {
 		return this.target && this.target.player !== this.piece.player;
@@ -214,18 +228,23 @@ class King extends Piece {
 	}
 	getActionPlans(gameBoard, opponentPlanBoard) {
 		const result = [];
-		const opponentPlans = opponentPlanBoard?.get(this.x, this.y).value?.arr;
 		A_DELTAS
-			.map(({dx,dy}) => this.player.coordinateCorrection(this.x, this.y, dx, dy))
-			.forEach(({xx, yy}) => {
+			.forEach(({dx, dy}) => {
+				const {xx, yy} = this.player.coordinateCorrection(this.x, this.y, dx, dy);
 				const {inside, value} = gameBoard.get(xx, yy);
 				if(!inside) return; //枠の外ならダメ
-				if(opponentPlanBoard?.get(xx, yy).value) return; //相手の移動(攻撃)範囲もしくはガードされている相手の駒ならダメ
-				if(opponentPlans?.find(plan => plan.extraStep?.x === xx && plan.extraStep?.y === yy)) return; //直線攻撃の同じ直線上はダメ
+				if(this.#isOpponentsAttackRange(xx, yy, opponentPlanBoard)) return;
 				result.push(new ActionPlan(xx, yy, this, value)); //移動・獲得・ガード
 			});
 		//TODO: キャスリング
 		return result;
+	}
+	//相手の行動(攻撃)範囲なら true
+	#isOpponentsAttackRange(xx, yy, opponentPlanBoard) {
+		if(!opponentPlanBoard) return false;
+		if(opponentPlanBoard.get(xx, yy).value) return true; //相手の移動(攻撃)範囲もしくはガードされている相手の駒ならダメ
+		const opponentPlans = opponentPlanBoard?.get(this.x, this.y).value?.arr;
+		return opponentPlans?.find(plan => plan.extraStep?.x === xx && plan.extraStep?.y === yy); //直線攻撃の同じ直線上はダメ
 	}
 }
 
@@ -233,8 +252,8 @@ class Queen extends Piece {
 	constructor(x, y, player) {
 		super(x, y, player, 4);
 	}
-	getActionPlans(gameBoard) {
-		return this.getStraightPlans(A_DELTAS, gameBoard);
+	getActionPlans(gameBoard, opponentPlanBoard) {
+		return this.getStraightPlans(A_DELTAS, gameBoard, opponentPlanBoard);
 	}
 }
 
@@ -242,8 +261,8 @@ class Bishop extends Piece {
 	constructor(x, y, player) {
 		super(x, y, player, 3);
 	}
-	getActionPlans(gameBoard) {
-		return this.getStraightPlans(X_DELTAS, gameBoard);
+	getActionPlans(gameBoard, opponentPlanBoard) {
+		return this.getStraightPlans(X_DELTAS, gameBoard, opponentPlanBoard);
 	}
 }
 
@@ -251,15 +270,16 @@ class Knight extends Piece {
 	constructor(x, y, player) {
 		super(x, y, player, 1);
 	}
-	getActionPlans(gameBoard) {
+	getActionPlans(gameBoard, opponentPlanBoard) {
 		const result = [];
 		K_DELTAS
-			.map(({dx,dy}) => this.player.coordinateCorrection(this.x, this.y, dx, dy))
-			.forEach(({xx, yy}) => {
+			.forEach(({dx, dy}) => {
+				const {xx, yy} = this.player.coordinateCorrection(this.x, this.y, dx, dy)
 				const {inside, value} = gameBoard.get(xx, yy);
-				if(inside) result.push(new ActionPlan(xx, yy, this, value)); //移動・獲得・ガード
+				if(!inside) return;
+				result.push(new ActionPlan(xx, yy, this, value)); //移動・獲得・ガード
 			});
-		return result;
+		return this.correctionDefense(result, gameBoard, opponentPlanBoard);
 	}
 }
 
@@ -267,8 +287,8 @@ class Rook extends Piece {
 	constructor(x, y, player) {
 		super(x, y, player, 2);
 	}
-	getActionPlans(gameBoard) {
-		return this.getStraightPlans(P_DELTAS, gameBoard);
+	getActionPlans(gameBoard, opponentPlanBoard) {
+		return this.getStraightPlans(P_DELTAS, gameBoard, opponentPlanBoard);
 	}
 }
 
@@ -278,25 +298,25 @@ class Pawn extends Piece {
 		this.firstMove = true; //ポーンは最初の一回だけ最大2マス進める
 		this.moveCount = 0;
 	}
-	getActionPlans(gameBoard) {
+	getActionPlans(gameBoard, opponentPlanBoard) {
 		const result = [];
 		const route = []; //現在地点は含まない
 		for(let i=0, xx=this.x, yy=this.y; i<(this.firstMove?2:1); i++) {
-			({xx, yy} = this.player.coordinateCorrection(xx, yy, 0, -1)); //一歩前
+			({xx, yy} = this.player.coordinateCorrection(xx, yy, 0, -1));
 			const {inside, value} = gameBoard.get(xx, yy);
-			if(!inside || value) break; //枠外もしくは何かに当たったならそこまで
-			result.push(new ActionPlan(xx, yy, this, undefined, route)); //移動
+			if(!inside || value) break; //枠外か何か居るならそこまで
+			result.push(new ActionPlan(xx, yy, this, value, route)); //移動
 			route.push({x:xx, y:yy});
 		}
 		//斜め前への獲得移動
 		[-1,1].forEach(dx => {
-			const {xx:x1, yy:y1} = this.player.coordinateCorrection(this.x, this.y, dx, -1);
-			let {inside, value} = gameBoard.get(x1, y1);
+			const {xx, yy} = this.player.coordinateCorrection(this.x, this.y, dx, -1);
+			let {inside, value} = gameBoard.get(xx, yy);
 			if(!inside) return; //枠外なら無視
 			if(!value) value = this.#getEnPassant(gameBoard, dx);
-			result.push(new ActionPlan(x1, y1, this, value).setCaptureOnly(true)); //普通に移動は出来ないけど一応戦闘範囲
+			result.push(new ActionPlan(xx, yy, this, value).setCaptureOnly(true)); //普通に移動は出来ないけど一応戦闘範囲
 		});
-		return result;
+		return this.correctionDefense(result, gameBoard, opponentPlanBoard);
 	}
 	//アンパッサン(en passant)
 	#getEnPassant(gameBoard, dx) {
@@ -322,23 +342,22 @@ const CANVAS_SIZE = TILE * BOARD_SIZE;
 const TILE_COLORS = Object.freeze(["#C8C8C8", "#64C8C8"]);
 
 class BoardDrawer {
+	#cx;
+	#cy;
+	#plans;
 	constructor(ctx) {
 		this.ctx = ctx;
 		this.ctx.imageSmoothingEnabled = false;
 		this.ctx.strokeStyle = "rgba(255, 255, 255, 255)";
-
-		this.cx = undefined;
-		this.cy = undefined;
-		this.plans = undefined;
 	}
 
 	setCursor(cx, cy) {
-		this.cx = cx;
-		this.cy = cy;
+		this.#cx = cx;
+		this.#cy = cy;
 	}
 
 	setActionPlans(plans) {
-		this.plans = plans;
+		this.#plans = plans;
 	}
 
 	draw(gameBoard) {
@@ -349,9 +368,9 @@ class BoardDrawer {
 				this.ctx.fillStyle = TILE_COLORS[i];
 				this.ctx.fillRect(x*TILE, y*TILE, TILE, TILE);
 
-				if(x == this.cx && y == this.cy) {
+				if(x == this.#cx && y == this.#cy) {
 					this.ctx.fillStyle = 'rgba(255, 255, 0, 50)';
-					this.ctx.fillRect(this.cx*TILE, this.cy*TILE, TILE, TILE);
+					this.ctx.fillRect(this.#cx*TILE, this.#cy*TILE, TILE, TILE);
 				}
 
 				const piece = gameBoard.get(x, y).value;
@@ -359,7 +378,7 @@ class BoardDrawer {
 			}
 		}
 		//プラン表示
-		this.plans?.forEach(plan => {
+		this.#plans?.forEach(plan => {
 			if(plan.isMovable()) this.#drawCircle(plan.x, plan.y, 'rgb(0, 255, 0)', 4.0);
 			if(plan.isCapture()) {
 				const target = plan.target;
@@ -408,36 +427,30 @@ function init() {
 	const ctx = canvas.getContext('2d');
 	const boardDrawer = new BoardDrawer(ctx);
 
-	//白コマ
-	const wpawn1 = new Pawn(3,5,Player.WHITE);
-	const wpawn2 = new Pawn(2,5,Player.WHITE);
-	const wknight1 = new Knight(1,4,Player.WHITE);
-	const wqueen = new Queen(2,7,Player.WHITE);
-	const wking = new King(4,6,Player.WHITE);
-	//黒コマ
-	const bpawn1 = new Pawn(4,3,Player.BLACK);
-	const brook1 = new Rook(0,6,Player.BLACK);
-	const bknight1 = new Knight(3,3,Player.BLACK);
-	const bbishop1 = new Bishop(7,4,Player.BLACK);
-	const bqueen = new Queen(4,0,Player.BLACK);
-	const bking = new King(3,0,Player.BLACK);
 	//配置
 	const gameBoard = new GameBoard();
-	gameBoard.put(wpawn1);
-	gameBoard.put(wpawn2);
-	gameBoard.put(wknight1);
-	gameBoard.put(wqueen);
-	gameBoard.put(wking);
-	gameBoard.put(bpawn1);
-	gameBoard.put(brook1);
-	gameBoard.put(bknight1);
-	gameBoard.put(bbishop1);
-	gameBoard.put(bqueen);
-	gameBoard.put(bking);
+	//白コマ
+	let player = Player.WHITE;
+	gameBoard.put(new Pawn(3, 5, player));
+	gameBoard.put(new Pawn(2, 5, player));
+	gameBoard.put(new Knight(1, 4, player));
+	gameBoard.put(new Bishop(6, 4, player));
+	gameBoard.put(new Queen(2, 7, player));
+	gameBoard.put(new King(4, 6, player));
+	//黒コマ
+	player = Player.BLACK;
+	gameBoard.put(new Pawn(1, 1, player));
+	gameBoard.put(new Pawn(2, 1, player));
+	gameBoard.put(new Pawn(4, 3, player));
+	gameBoard.put(new Rook(0, 6, player));
+	gameBoard.put(new Knight(3, 3, player));
+	gameBoard.put(new Bishop(7, 4, player));
+	gameBoard.put(new Queen(4, 0, player));
+	gameBoard.put(new King(3, 1, player));
 
 	const planManager = new PlanManager(gameBoard);
 
-	let player = Player.WHITE;
+	player = Player.WHITE;
 	planManager.rebuildPlan(player);
 
 	const button = document.getElementById('button');
